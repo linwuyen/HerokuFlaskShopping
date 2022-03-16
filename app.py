1a45e79521acfdb86f1ec0f58f9681434b10ce83
linwuyen <zerofivem@gmail.com>
2022-03-17 02:14:10 +0800

    up

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,make_response
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static/imges')
+IMG_PATH=""
 
 @app.route("/")
-
 def index():
     return render_template("index.html")
 
